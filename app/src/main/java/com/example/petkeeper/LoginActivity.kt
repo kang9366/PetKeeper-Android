6a16a044a58d9f9a46197429227b8d88cafe2fc1
SeungGu Kang <alex9366@naver.com>
2023-04-24 02:49:39 +0900

    뒤로가기를 두번 누르면 앱이 종료되도록 구현

--- app/src/main/java/com/example/petkeeper/LoginActivity.kt
+++ app/src/main/java/com/example/petkeeper/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.example.petkeeper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.petkeeper.databinding.ActivityLoginBinding
+import com.example.petkeeper.databinding.ActivityMainBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
