a3020535ba916de3a2aa99ff9a46a0d25391377f
SeungGu Kang <alex9366@naver.com>
2023-05-04 12:04:55 +0900

    메인 화면 UI 구현

--- app/src/main/java/com/example/petkeeper/MainFragment.kt
+++ app/src/main/java/com/example/petkeeper/MainFragment.kt
@@ -1,59 +1,48 @@
 package com.example.petkeeper
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.petkeeper.databinding.FragmentMainBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        binding = FragmentMainBinding.inflate(layoutInflater)
+        val data = this.arguments?.getString("test")
+        Log.d("test", data.toString())
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recyclerView = binding.recyclerView
+        val item = ArrayList<RecyclerViewData>()
+
+        item.add(RecyclerViewData("월", "1"))
+        item.add(RecyclerViewData("화", "2"))
+        item.add(RecyclerViewData("수", "3"))
+        item.add(RecyclerViewData("목", "4"))
+        item.add(RecyclerViewData("금", "5"))
+        item.add(RecyclerViewData("토", "6"))
+        item.add(RecyclerViewData("일", "7"))
+
+        val adapter = RecyclerViewAdapter(item)
+//        val layoutManager = LinearLayoutManager(context)
+//        layoutManager.orientation = LinearLayoutManager.HORIZONTAL
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+//            setLayoutManager(layoutManager)
+            this.adapter = adapter
+        }
     }
 }
