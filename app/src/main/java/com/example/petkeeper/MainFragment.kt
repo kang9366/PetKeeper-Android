019001cff0cfdacba2a7a291ad3fb955937a4a3c
SeungGu Kang <alex9366@naver.com>
2023-05-06 11:48:29 +0900

    RecyclerView -> GridLayout으로 대체

--- app/src/main/java/com/example/petkeeper/MainFragment.kt
+++ app/src/main/java/com/example/petkeeper/MainFragment.kt
@@ -1,48 +1,63 @@
 package com.example.petkeeper
 
+import android.animation.ObjectAnimator
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.petkeeper.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
+    private lateinit var name: String
+    private var isFabOpen = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMainBinding.inflate(layoutInflater)
-        val data = this.arguments?.getString("test")
-        Log.d("test", data.toString())
+
         return binding.root
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val recyclerView = binding.recyclerView
-        val item = ArrayList<RecyclerViewData>()
 
-        item.add(RecyclerViewData("월", "1"))
-        item.add(RecyclerViewData("화", "2"))
-        item.add(RecyclerViewData("수", "3"))
-        item.add(RecyclerViewData("목", "4"))
-        item.add(RecyclerViewData("금", "5"))
-        item.add(RecyclerViewData("토", "6"))
-        item.add(RecyclerViewData("일", "7"))
+        binding.nameText.text = name
 
-        val adapter = RecyclerViewAdapter(item)
-//        val layoutManager = LinearLayoutManager(context)
-//        layoutManager.orientation = LinearLayoutManager.HORIZONTAL
-        recyclerView.apply {
-            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-//            setLayoutManager(layoutManager)
-            this.adapter = adapter
+        binding.fabMain.apply {
+            bringToFront()
+            setOnClickListener {
+                toggleFab()
+            }
         }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        name = arguments?.getString("name").toString()
+    }
+
+    private fun toggleFab() {
+        if (isFabOpen) {
+            ObjectAnimator.ofFloat(binding.fabSub1, "translationY", 0f).apply { start() }
+            ObjectAnimator.ofFloat(binding.fabSub2, "translationY", 0f).apply { start() }
+            binding.fabMain.setImageResource(R.drawable.ic_up)
+        } else {
+            ObjectAnimator.ofFloat(binding.fabSub1, "translationY", -200f).apply { start() }
+            ObjectAnimator.ofFloat(binding.fabSub2, "translationY", -400f).apply { start() }
+            binding.fabMain.setImageResource(R.drawable.ic_down)
+        }
+        isFabOpen = !isFabOpen
     }
 }
