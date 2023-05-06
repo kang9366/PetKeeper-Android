5791ec82b192a0580894ba10c1b6699ca825f734
SeungGu Kang <alex9366@naver.com>
2023-05-06 11:44:37 +0900

    현재 위치 (위도, 경도) 받아오는 기능 구현

--- app/src/main/java/com/example/petkeeper/MapFragment.kt
+++ app/src/main/java/com/example/petkeeper/MapFragment.kt
@@ -1,15 +1,39 @@
 package com.example.petkeeper
 
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.app.Activity
+import android.content.Context
+import android.content.Context.LOCATION_SERVICE
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat.checkSelfPermission
 import com.example.petkeeper.databinding.FragmentMapBinding
 import net.daum.mf.map.api.MapView
 
 class MapFragment : Fragment() {
     private lateinit var binding: FragmentMapBinding
+    private lateinit var context: Context
+    private lateinit var locationManager: LocationManager
+    private lateinit var myLocationListener: MyLocationListener
+
+    override fun onAttach(p0: Context) {
+        super.onAttach(p0)
+        context = p0 as MainActivity
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +45,82 @@ class MapFragment : Fragment() {
         val mapViewContainer = binding.mapView
         mapViewContainer.addView(mapView)
 
+        binding.button.setOnClickListener {
+            getMylocation()
+        }
+
         return binding.root
     }
+
+
+
+    companion object {
+        val locationPermissions = arrayOf(
+            ACCESS_FINE_LOCATION,
+            ACCESS_COARSE_LOCATION
+        )
+    }
+
+    private val permissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            val responsePermissions = permissions.entries.filter {
+                it.key in locationPermissions
+            }
+
+            if (responsePermissions.filter { it.value == true }.size == locationPermissions.size) {
+                setLocationListener()
+            } else {
+                Toast.makeText(context, "no", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    private fun getMylocation() {
+        if (::locationManager.isInitialized.not()) {
+            locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        }
+        val isGpsEnable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        if (isGpsEnable) {
+            permissionLauncher.launch(locationPermissions)
+        }
+    }
+
+    @Suppress("MissingPermission")
+    private fun setLocationListener() {
+        val minTime: Long = 1500
+        val minDistance = 100f
+
+        if (::myLocationListener.isInitialized.not()) {
+            myLocationListener = MyLocationListener()
+        }
+
+        with(locationManager) {
+            requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                minTime, minDistance, myLocationListener
+            )
+
+            requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER,
+                minTime, minDistance, myLocationListener
+            )
+        }
+    }
+
+    inner class MyLocationListener : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            Toast
+                .makeText(context, "${location.latitude}, ${location.longitude}", Toast.LENGTH_SHORT)
+                .show()
+
+            removeLocationListener()
+        }
+
+        private fun removeLocationListener() {
+            if (::locationManager.isInitialized && ::myLocationListener.isInitialized) {
+                locationManager.removeUpdates(myLocationListener)
+            }
+        }
+    }
+
+
 }
