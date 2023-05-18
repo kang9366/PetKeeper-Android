b52be39be8558a9b08c02449f7e0c199a2db3fe3
SeungGu Kang <alex9366@naver.com>
2023-05-19 00:53:24 +0900

    사진 post 기능 추가

--- app/src/main/java/com/example/petkeeper/MainFragment.kt
+++ app/src/main/java/com/example/petkeeper/MainFragment.kt
@@ -1,5 +1,7 @@
 package com.example.petkeeper
 
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.animation.ObjectAnimator
 import android.app.Activity
 import android.content.Context
@@ -7,6 +9,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
@@ -18,9 +21,21 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker.checkPermission
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.petkeeper.databinding.FragmentMainBinding
+import com.google.gson.JsonObject
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
@@ -38,9 +53,6 @@ class MainFragment : Fragment() {
         return binding.root
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -60,6 +72,10 @@ class MainFragment : Fragment() {
                 initAddPhoto()
             }
         }
+
+        binding.fabSub1.setOnClickListener {
+            initCamera()
+        }
     }
 
     override fun onAttach(context: Context) {
@@ -75,7 +91,7 @@ class MainFragment : Fragment() {
         if (isFabOpen) {
             ObjectAnimator.ofFloat(binding.fabSub1, "translationY", 0f).apply { start() }
             ObjectAnimator.ofFloat(binding.fabSub2, "translationY", 0f).apply { start() }
-            binding.fabMain.setImageResource(R.drawable.ic_up)
+            binding.fabMain.setImageResource(R.drawable.ic_examination)
         } else {
             ObjectAnimator.ofFloat(binding.fabSub1, "translationY", -200f).apply { start() }
             ObjectAnimator.ofFloat(binding.fabSub2, "translationY", -400f).apply { start() }
@@ -84,23 +100,44 @@ class MainFragment : Fragment() {
         isFabOpen = !isFabOpen
     }
 
-    private fun initAddPhoto(){
-        //갤러리 접근 권한 설정
+    private fun initCamera() {
         when {
             ContextCompat.checkSelfPermission(
                 mainActivity,
-                android.Manifest.permission.READ_EXTERNAL_STORAGE
+                CAMERA
             ) == PackageManager.PERMISSION_GRANTED -> {
-                //권한이 부여되었을 때 갤러리에서 사진 선택
-                navigatePhoto()
+                runCamera()
             }
-            shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
-                showPermissionContextPopup()
+            shouldShowRequestPermissionRationale(CAMERA) -> {
+                showPermissionContextPopup(CAMERA)
             }
             else -> {
-                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+                requestPermissions(arrayOf(CAMERA), 1000)
             }
         }
+    }
+
+    private fun initAddPhoto(){
+        when {
+            ContextCompat.checkSelfPermission(
+                mainActivity,
+                READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                navigatePhoto()
+            }
+            shouldShowRequestPermissionRationale(READ_EXTERNAL_STORAGE) -> {
+                showPermissionContextPopup(READ_EXTERNAL_STORAGE)
+            }
+            else -> {
+                requestPermissions(arrayOf(READ_EXTERNAL_STORAGE), 1000)
+            }
+        }
+    }
+
+    fun rotateBitmap(bitmap: Bitmap, degrees: Float): Bitmap {
+        val matrix = Matrix()
+        matrix.postRotate(degrees)
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
     }
 
     @Deprecated("Deprecated in Java")
@@ -109,37 +146,86 @@ class MainFragment : Fragment() {
         if(resultCode != Activity.RESULT_OK){
             return
         }
+        val selectedImageUri: Uri? = data?.data
 
         when(requestCode) {
             2000 -> {
-                val selectedImageUri: Uri? = data?.data
                 if(selectedImageUri != null){
                     binding.dogImage.setImageURI(selectedImageUri)
                 }else{
                     Toast.makeText(mainActivity, "사진을 가져오지 못했습니다", Toast.LENGTH_SHORT).show()
                 }
             }
+
+            3000 -> {
+                val extras = data?.extras
+                val bitmap = extras?.get("data") as Bitmap?
+                val rotatedBitmap = rotateBitmap(bitmap!!, 90f)
+                val file: File = convertBitmapToFile(rotatedBitmap)
+
+                val survey = RequestBody.create("image/*".toMediaTypeOrNull(), file)
+                val multipart = MultipartBody.Part.createFormData("image", "download.jpg", survey)
+                postImage(multipart)
+            }
+
             else -> {
                 Toast.makeText(mainActivity, "사진을 가져오지 못했습니다", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    private fun showPermissionContextPopup() {
+    private fun showPermissionContextPopup(permission: String) {
         AlertDialog.Builder(mainActivity)
             .setTitle("권한이 필요합니다.")
             .setMessage("사진을 등록하기 위해서 권한이 필요합니다.")
-            .setPositiveButton("동의하기", {dialog, which ->
-                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
-            })
-            .setNegativeButton("취소하기", {_, _-> })
+            .setPositiveButton("동의하기") { _, _ ->
+                requestPermissions(arrayOf(permission), 1000)
+            }
+            .setNegativeButton("취소하기") { _, _ -> }
             .create()
             .show()
     }
 
     private fun navigatePhoto(){
-        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
         startActivityForResult(intent, 2000)
+    }
+
+    private fun runCamera(){
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, 3000)
+    }
+
+    private fun getAbsolutePathFromUri(uri: Uri, context: Context): String? {
+        val projection = arrayOf(MediaStore.Images.Media.DATA)
+        var path: String? = null
+        context.contentResolver.query(uri, projection, null, null, null)?.use { cursor ->
+            if (cursor.moveToFirst()) {
+                val columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+                path = cursor.getString(columnIndex)
+            }
+        }
+        return path
+    }
+
+    private fun convertBitmapToFile(bitmap: Bitmap): File{
+        val file = File(mainActivity.filesDir, "picture")
+        val output = FileOutputStream(file)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, output)
+        return file
+    }
+
+    private fun postImage(body: MultipartBody.Part){
+        RetrofitBuilder.api.postEyeImage(body).enqueue(object: Callback<JsonObject> {
+            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+
+                Log.d("post eye image", response.toString())
+            }
+
+            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                Log.d("post eye image", t.message.toString())
+            }
+        })
     }
 }
