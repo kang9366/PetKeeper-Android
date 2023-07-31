588ba27189d5d60a4397f635667985198b0e11bc
SeungGu Kang <alex9366@naver.com>
2023-08-01 01:34:20 +0900

    Update CommunityFragment.kt : 글쓰기 버튼 클릭시 dialog 실행

--- app/src/main/java/com/example/petkeeper/CommunityFragment.kt
+++ app/src/main/java/com/example/petkeeper/CommunityFragment.kt
@@ -1,13 +1,16 @@
 package com.example.petkeeper
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import android.view.Window
+import androidx.appcompat.app.AlertDialog
 import com.example.petkeeper.databinding.FragmentCommunityBinding
-import com.example.petkeeper.databinding.FragmentMainBinding
 
 class CommunityFragment : Fragment() {
     private lateinit var binding: FragmentCommunityBinding
@@ -24,15 +27,36 @@ class CommunityFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val recyclerView = binding.recyclerView
         val item = ArrayList<Data>()
-        item.add(Data("test"))
-        item.add(Data("test"))
-        item.add(Data("test"))
-        item.add(Data("test"))
-        item.add(Data("test"))
-        item.add(Data("test"))
-        item.add(Data("test"))
+
+        for(i in 0..8){
+            item.add(Data("test"))
+        }
 
         val adapter = RecyclerViewAdapter(item)
         recyclerView.adapter = adapter
+
+        binding.postButton.setImageResource(R.drawable.post)
+        initPostDialog()
+    }
+
+    private fun initPostDialog(){
+        binding.postButton.setOnClickListener{
+            val dialogView = layoutInflater.inflate(R.layout.post_dialog, null)
+            val dialog = AlertDialog.Builder(requireContext())
+                .setView(dialogView)
+                .create()
+
+            dialog.setOnShowListener {
+                dialog.window?.apply {
+                    setLayout(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT)
+                    setGravity(Gravity.BOTTOM)
+                    setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                }
+            }
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.show()
+        }
     }
 }
