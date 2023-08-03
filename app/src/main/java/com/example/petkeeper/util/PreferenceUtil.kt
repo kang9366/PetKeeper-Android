90d9020281f86bb13514cee11780fa53ed306f81
SeungGu Kang <alex9366@naver.com>
2023-08-03 14:42:29 +0900

    Update PreferenceUtil.kt : SharedPreference 추가

--- app/src/main/java/com/example/petkeeper/util/PreferenceUtil.kt
+++ app/src/main/java/com/example/petkeeper/util/PreferenceUtil.kt
@@ -4,5 +4,13 @@ import android.content.Context
 import android.content.SharedPreferences
 
 class PreferenceUtil(context: Context) {
-    private val preferences: SharedPreferences = context.getSharedPreferences("pref", Context.MODE_PRIVATE)
+    private val pref: SharedPreferences = context.getSharedPreferences("pref", Context.MODE_PRIVATE)
+
+    var isRegistered: Boolean
+        get() = pref.getBoolean("isRegistered", false)
+        set(value) = pref.edit().putBoolean("isRegistered", value).apply()
+
+    var token: String?
+        get() = pref.getString("token", null)
+        set(value) = pref.edit().putString("token", value).apply()
 }
