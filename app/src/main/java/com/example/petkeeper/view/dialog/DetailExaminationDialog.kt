4670ae66f43d972158d018aa81f18e18a97d1fda
SeungGu Kang <alex9366@naver.com>
2023-09-28 00:56:45 +0900

    update DetailExaminationDialog.kt

--- app/src/main/java/com/example/petkeeper/view/dialog/DetailExaminationDialog.kt
+++ app/src/main/java/com/example/petkeeper/view/dialog/DetailExaminationDialog.kt
@@ -1,17 +1,15 @@
 package com.example.petkeeper.view.dialog
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.example.petkeeper.R
 import com.example.petkeeper.databinding.DetailExaminationDialogBinding
 import com.example.petkeeper.util.binding.BindingFragment
+import com.mcdev.quantitizerlibrary.AnimationStyle
 
 class DetailExaminationDialog : BindingFragment<DetailExaminationDialogBinding>(R.layout.detail_examination_dialog, false) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding?.numberPicker?.textAnimationStyle = AnimationStyle.SLIDE_IN
     }
 }
