734dd74d4f5f2e6a48d2d79fee334619546f3bff
SeungGu Kang <alex9366@naver.com>
2023-10-10 16:16:15 +0900

    Update DetailDialog.kt : 몸무게 정보 post api 연결

--- app/src/main/java/com/example/petkeeper/view/dialog/DetailDialog.kt
+++ app/src/main/java/com/example/petkeeper/view/dialog/DetailDialog.kt
@@ -2,14 +2,21 @@ package com.example.petkeeper.view.dialog
 
 import android.annotation.SuppressLint
 import android.app.Dialog
+import android.util.Log
 import android.view.ViewGroup
 import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
 import com.example.petkeeper.databinding.DetailDialogBinding
+import com.example.petkeeper.util.App
 import com.example.petkeeper.util.adapter.DateItem
 import com.example.petkeeper.util.adapter.ViewPagerAdapter
+import com.example.petkeeper.util.api.RetrofitBuilder
+import com.google.gson.JsonObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class DetailDialog(context : AppCompatActivity) {
+class DetailDialog(context : AppCompatActivity, private val viewModel: TestViewModel) {
     private val binding = DetailDialogBinding.inflate(context.layoutInflater)
     private val dialog = Dialog(context)
     private val viewPagerAdapter = ViewPagerAdapter(context)
@@ -35,6 +42,24 @@ class DetailDialog(context : AppCompatActivity) {
 
         binding.closeButton.setOnClickListener {
             dialog.dismiss()
+        }
+
+        binding.confirmButton.setOnClickListener {
+            Log.d("testtt", App.preferences.Pet().id.toString())
+            RetrofitBuilder.api.postWeight(PET_WEIGHT=viewModel.weight.value.toString(), PET_WEIGHT_DATE = binding.titleText.text.toString()).enqueue(object :
+                Callback<JsonObject> {
+                override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                    if(response.isSuccessful){
+                        Log.d("testtt", response.body().toString())
+                    }else{
+                        Log.d("testtt", response.body().toString())
+                    }
+                }
+
+                override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                    Log.d("testtt", t.message.toString())
+                }
+            })
         }
 
         binding.cancelButton.setOnClickListener {
