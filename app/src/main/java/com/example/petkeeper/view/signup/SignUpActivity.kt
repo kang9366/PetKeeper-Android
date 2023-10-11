156df5086bc68c9862a8c0f183d3c27e9ae92bc8
SeungGu Kang <alex9366@naver.com>
2023-10-12 03:49:06 +0900

    Update SignUpActivity.kt: 회원가입 기능 구현 및 DataBinding 설정

--- app/src/main/java/com/example/petkeeper/view/signup/SignUpActivity.kt
+++ app/src/main/java/com/example/petkeeper/view/signup/SignUpActivity.kt
@@ -1,21 +1,52 @@
 package com.example.petkeeper.view.signup
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MotionEvent
-import android.view.inputmethod.InputMethodManager
+import android.view.View
+import android.widget.Toast
 import com.example.petkeeper.R
+import com.example.petkeeper.databinding.ActivitySignUpBinding
+import com.example.petkeeper.model.User
+import com.example.petkeeper.util.api.RetrofitBuilder
+import com.example.petkeeper.util.binding.BindingActivity
+import com.google.gson.JsonObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class SignUpActivity : AppCompatActivity() {
+
+class SignUpActivity : BindingActivity<ActivitySignUpBinding>(R.layout.activity_sign_up) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding.data = this
     }
 
-    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
-        val imm: InputMethodManager =
-            getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
-        return super.dispatchTouchEvent(ev)
+    fun initSignUp(view: View){
+        if(binding.editName.text.isNotEmpty() &&
+            binding.editEmail.text.isNotEmpty() &&
+            binding.editPhone.text.isNotEmpty() &&
+            (binding.editPassword.text.toString() == binding.editCheckPassword.text.toString())){
+            postData(binding.editEmail.text.toString(), binding.editCheckPassword.text.toString(), binding.editPhone.text.toString())
+            finish()
+        }else{
+            Toast.makeText(this, "모든 정보를 입력해주세요!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun postData(email: String, password: String, phone: String){
+        RetrofitBuilder.api.postSignUpData(user = User(email, password, phone)).enqueue(object: Callback<JsonObject>{
+            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                if(response.isSuccessful){
+                    if(response.code()==200){
+                        Toast.makeText(this@SignUpActivity, "회원가입 성공", Toast.LENGTH_SHORT).show()
+                    }else{
+                        Toast.makeText(this@SignUpActivity, "이메일이 중복되었습니다", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+//                Log.d("Sign Up", t.message.toString())
+            }
+        })
     }
 }
