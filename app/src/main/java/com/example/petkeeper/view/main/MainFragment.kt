7a591281cfec8c6c70da66f780dac159eb555a2d
SeungGu Kang <alex9366@naver.com>
2023-10-27 13:18:38 +0900

    Update LoginActivity.kt: preference에 pet id 추가

--- app/src/main/java/com/example/petkeeper/view/main/MainFragment.kt
+++ app/src/main/java/com/example/petkeeper/view/main/MainFragment.kt
@@ -1,29 +1,20 @@
 package com.example.petkeeper.view.main
 
-import android.Manifest.permission.CAMERA
-import android.Manifest.permission.READ_EXTERNAL_STORAGE
-import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.Matrix
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
-import android.provider.MediaStore
 import android.util.Log
 import android.view.View
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AlertDialog
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
+import com.bumptech.glide.Glide
 import com.example.petkeeper.R
 import com.example.petkeeper.util.api.RetrofitBuilder
 import com.example.petkeeper.databinding.FragmentMainBinding
+import com.example.petkeeper.model.PetVaccination
+import com.example.petkeeper.model.PetWeight
 import com.example.petkeeper.model.UserResponse
 import com.example.petkeeper.util.App.Companion.preferences
 import com.example.petkeeper.util.adapter.DateAdapter
@@ -34,17 +25,14 @@ import com.example.petkeeper.view.dialog.DetailDialog
 import com.example.petkeeper.view.dialog.TestViewModel
 import com.example.petkeeper.view.dialog.VaccinationDialog
 import com.example.petkeeper.view.dialog.WeightDialog
-import com.google.android.gms.common.wrappers.Wrappers
-import com.google.gson.JsonObject
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.MultipartBody
-import okhttp3.RequestBody
-import okhttp3.RequestBody.Companion.asRequestBody
-import okhttp3.RequestBody.Companion.toRequestBody
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
@@ -57,10 +45,10 @@ import java.util.Locale
 class MainFragment : BindingFragment<FragmentMainBinding>(R.layout.fragment_main, true) {
     private val viewModel: TestViewModel by activityViewModels()
 
-    private var isFabOpen = false
     private lateinit var context: MainActivity
     private var item = ArrayList<DateItem>()
     private val calendar = Calendar.getInstance()
+    private lateinit var weightData: List<PetWeight>
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -75,18 +63,6 @@ class MainFragment : BindingFragment<FragmentMainBinding>(R.layout.fragment_main
         initRecyclerView()
         getUserData()
 
-
-        binding?.fabMain?.apply {
-            bringToFront()
-            setOnClickListener {
-                toggleFab()
-            }
-        }
-
-        binding?.fabSub1?.setOnClickListener {
-            requestCameraPermission()
-        }
-
         binding?.vaccinationDateText?.setOnClickListener {
             val dialog = VaccinationDialog(context)
             dialog.initDialog()
@@ -98,10 +74,61 @@ class MainFragment : BindingFragment<FragmentMainBinding>(R.layout.fragment_main
         }
     }
 
+    private fun initWeightChart() {
+        val entries = ArrayList<Entry>()
+
+        for ((index, weightDataItem) in weightData.withIndex()) {
+            val weight = weightDataItem.PET_WEIGHT.toFloat()
+            val date = weightDataItem.PET_WEIGHT_DATE
+            entries.add(Entry(index.toFloat(), weight))
+        }
+
+        val dataSet = LineDataSet(entries, null)
+        dataSet.color = Color.BLACK
+        dataSet.valueTextColor = Color.RED
+        dataSet.valueTextSize = 11f
+
+        dataSet.lineWidth = 2.5f
+
+        dataSet.circleRadius = 6f
+        dataSet.setDrawCircles(true)
+
+        val lineChart = binding!!.lineChart
+        lineChart.data = LineData(dataSet)
+
+        val xAxis = lineChart.xAxis
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+
+        val xLabels = ArrayList<String>()
+        for (weightDataItem in weightData) {
+            xLabels.add(weightDataItem.PET_WEIGHT_DATE)
+        }
+
+        xAxis.valueFormatter = IndexAxisValueFormatter(xLabels.toTypedArray())
+        xAxis.setLabelCount(xLabels.size, true)
+
+        lineChart.axisLeft.isEnabled = false
+        lineChart.axisRight.isEnabled = false
+
+        lineChart.description.isEnabled = false
+        lineChart.legend.isEnabled = false
+
+        lineChart.invalidate()
+    }
+
     private fun getUserData(){
         RetrofitBuilder.api.getUserInfo(userId=preferences.userId.toString()).enqueue(object : Callback<UserResponse>{
             override fun onResponse(call: Call<UserResponse>, response: Response<UserResponse>) {
                 val responseData = response.body()!!.p_pets[0]
+                Log.d("testttt", responseData.p_pet_vaccinations.toString())
+                weightData = responseData.p_pet_weights
+                Log.d("weight test", weightData.toString())
+                initWeightChart()
+                binding?.vaccinationDateText?.text = responseData.p_pet_vaccinations[0].PET_VACCINATION_DATE
+                Glide.with(requireContext())
+                    .load(responseData.PET_IMAGE)
+                    .into(binding?.dogImage!!)
+                binding?.weightText?.text = responseData.p_pet_weights.last().PET_WEIGHT.toString() + "kg"
                 binding?.nameText?.text = responseData.PET_NAME
                 binding?.ageText?.text = responseData.PET_BIRTHDATE
                 binding?.breedText?.text = responseData.PET_KIND
@@ -170,99 +197,6 @@ class MainFragment : BindingFragment<FragmentMainBinding>(R.layout.fragment_main
             }
         })
         binding?.dateRecyclerView?.adapter = adapter
-    }
-
-    private fun toggleFab() {
-        if (isFabOpen) {
-            ObjectAnimator.ofFloat(binding?.fabSub1, "translationY", 0f).apply { start() }
-            ObjectAnimator.ofFloat(binding?.fabSub2, "translationY", 0f).apply { start() }
-            binding?.fabMain?.setImageResource(R.drawable.ic_examination)
-        } else {
-            ObjectAnimator.ofFloat(binding?.fabSub1, "translationY", -250f).apply { start() }
-            ObjectAnimator.ofFloat(binding?.fabSub2, "translationY", -500f).apply { start() }
-            binding?.fabMain?.setImageResource(R.drawable.ic_down)
-        }
-        isFabOpen = !isFabOpen
-    }
-
-    private fun requestCameraPermission() {
-        when (PackageManager.PERMISSION_GRANTED) {
-            ContextCompat.checkSelfPermission(requireContext(), CAMERA) -> {
-                initCamera()
-            }
-            else -> {
-                cameraPermissionLauncher.launch(CAMERA)
-            }
-        }
-    }
-
-    @SuppressLint("QueryPermissionsNeeded")
-    private fun initCamera() {
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        if (takePictureIntent.resolveActivity(requireContext().packageManager) != null) {
-            takePictureLauncher.launch(takePictureIntent)
-        }
-    }
-
-    fun saveBitmapToFile(context: Context, bitmap: Bitmap): File? {
-        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
-        val fileName = "IMG_$timeStamp.jpg"
-
-        val storageDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-
-        return try {
-            val file = File(storageDir, fileName)
-            val fos = FileOutputStream(file)
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
-            fos.close()
-            file
-        } catch (e: IOException) {
-            e.printStackTrace()
-            null
-        }
-    }
-
-    private val takePictureLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == Activity.RESULT_OK) {
-            val bitmap = result.data?.extras?.get("data") as Bitmap
-            val file = saveBitmapToFile(context, bitmap)
-            val fileRequestBody = file!!.asRequestBody("application/octet-stream".toMediaTypeOrNull())
-            val filePart = MultipartBody.Part.createFormData("image", file.name, fileRequestBody)
-            postImage(filePart)
-        }
-    }
-
-    private val cameraPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { isGranted ->
-        if (isGranted) {
-            initCamera()
-        } else {
-            // 권한을 거부한 경우
-        }
-    }
-
-    private fun bitmapToMultipart(bitmap: Bitmap, fileName: String): MultipartBody.Part {
-        val byteArrayOutputStream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 10, byteArrayOutputStream)
-        val byteArray = byteArrayOutputStream.toByteArray()
-
-        val requestBody = byteArray.toRequestBody("image/jpeg".toMediaTypeOrNull(), 0, byteArray.size)
-        return MultipartBody.Part.createFormData("image ", fileName, requestBody)
-    }
-
-
-
-    private fun postImage(body: MultipartBody.Part){
-        RetrofitBuilder.api.postEyeImage(image = body).enqueue(object: Callback<JsonObject> {
-            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
-                Log.d("post eye image", response.body().toString())
-            }
-
-            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
-                Log.d("post eye image error", t.message.toString())
-            }
-        })
     }
 
     private fun getDayOfWeek(dayOfWeek: Int): String {
