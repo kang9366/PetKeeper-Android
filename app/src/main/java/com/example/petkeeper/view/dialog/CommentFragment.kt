35eef788f583772d36449372dbafad768332e373
SeungGu Kang <alex9366@naver.com>
2023-10-27 13:10:47 +0900

    Add fragment_result.xml : 결과 화면 표시를 위한 ui 추가

---
+++ app/src/main/java/com/example/petkeeper/view/dialog/CommentFragment.kt
@@ -0,0 +1,13 @@
+package com.example.petkeeper.view.dialog
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.petkeeper.R
+import com.example.petkeeper.util.binding.BindingFragment
+
+//class CommentFragment : BindingFragment<>(R.layout.fragment_comment, false) {
+//
+//}
